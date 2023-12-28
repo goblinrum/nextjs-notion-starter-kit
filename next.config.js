144ae48d39dc1c610bb4bfca130f31342d5b017e
goblinrum <ryan.ma3011@berkeley.edu>
2023-12-28 02:22:06 -0600

    added drive

--- next.config.js
+++ next.config.js
@@ -13,7 +13,8 @@ module.exports = withBundleAnalyzer({
       'pbs.twimg.com',
       'abs.twimg.com',
       's3.us-west-2.amazonaws.com',
-      'yutengma.me'
+      'yutengma.me',
+      'drive.google.com'
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
